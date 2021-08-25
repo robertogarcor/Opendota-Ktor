4efac6d25941060c34e18742f94773096d09ec3e
Roberto Garcia <groberto74@gmail.com>
2021-08-25 22:27:49 +0200

    group module heroes

--- src/Application.kt
+++ src/Application.kt
@@ -1,47 +1,8 @@
 package com.example.rgc.opendotaktor
 
-import com.example.rgc.opendotaktor.domain.HeroStatsRepository
-import com.example.rgc.opendotaktor.domain.HeroStatsRepositoryImpl
-import com.example.rgc.opendotaktor.domain.getHeroes
-import com.example.rgc.opendotaktor.local.OpenDotaLocalDataSource
-import com.example.rgc.opendotaktor.server.OpenDotaRemoteDataSource
-import com.example.rgc.opendotaktor.utils.EntityMapperImpl
-import io.ktor.application.*
-import io.ktor.routing.*
-import io.ktor.gson.*
-import io.ktor.features.*
-import io.ktor.client.*
-import io.ktor.client.engine.apache.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.logging.*
-
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.hero(testing: Boolean = false) {
-    install(ContentNegotiation) {
-        gson {
-        }
-    }
 
-    val client = HttpClient(Apache) {
-        install(JsonFeature) {
-            serializer = GsonSerializer()
-        }
-        install(Logging) {
-            //logger = null
-            //level = LogLevel.BODY
-        }
-    }
-
-    val repository: HeroStatsRepository = HeroStatsRepositoryImpl(client,
-                                            OpenDotaRemoteDataSource(EntityMapperImpl()),
-                                            OpenDotaLocalDataSource(EntityMapperImpl()))
-    routing {
-        this.getHeroes(repository)
-    }
-}
 
 
 
