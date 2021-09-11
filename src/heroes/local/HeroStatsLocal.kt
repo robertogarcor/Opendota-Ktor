6116315d06f8909902c5f333328d4fe871493c2f
Roberto Garcia <groberto74@gmail.com>
2021-09-11 15:21:19 +0200

    Create HeroStats table model Exposed

--- src/heroes/local/HeroStatsLocal.kt
+++ src/heroes/local/HeroStatsLocal.kt
@@ -1,5 +1,8 @@
 package com.example.rgc.opendotaktor.heroes.local
 
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+
 data class HeroStatsLocal(
     val id: Int,
     val name: String,
@@ -13,4 +16,14 @@ data class HeroStatsLocal(
         var heroStats : List<HeroStatsLocal> = ArrayList()
     }
 
+}
+
+object HeroStats : Table() {
+    private val id: Column<Int> = integer("id").autoIncrement()
+    val name: Column<String> = varchar("name", 100)
+    val localizedName: Column<String> = varchar("localizedName", 100)
+    val primaryAttr: Column<String> = varchar("primaryAttr", 100)
+    val attackType: Column<String> = varchar("attackType", 100)
+    val image: Column<String> = varchar("image", 250)
+    override val primaryKey = PrimaryKey(id, name = "id")
 }
