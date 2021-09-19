fce097aaeb3af90a9c0bc4e309f31c91facb5554
Roberto Garcia <groberto74@gmail.com>
2021-09-19 11:21:53 +0200

    Update User local model. Add id and fun map entity resultRow to userlocal

--- src/users/local/UserLocal.kt
+++ src/users/local/UserLocal.kt
@@ -5,9 +5,11 @@
 
 package com.example.rgc.opendotaktor.users.local
 
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
 
-data class UserLocal(val username : String,
+data class UserLocal(val id : Int,
+                     val username : String,
                      val password : String,
                      val firstname : String,
                      val email : String,
@@ -15,12 +17,23 @@ data class UserLocal(val username : String,
 
 
 object Users : Table("users") {
-    private val id = integer("id").autoIncrement()
+    val userId = integer("id").autoIncrement()
     val username = varchar("username", 100).uniqueIndex()
     val password = varchar("password", 255)
     val firstname = varchar("firstname", 100)
-    val email = varchar("email", 100)
+    val email = varchar("email", 100).uniqueIndex()
     val description = varchar("description", 512)
-    override val primaryKey = PrimaryKey(id, name = "id")
+    override val primaryKey = PrimaryKey(userId, name = "pk_user")
 
+}
+
+fun mapperToUserLocal(dbModel : ResultRow) : UserLocal {
+    return UserLocal(
+        id = dbModel[Users.userId],
+        username = dbModel[Users.username],
+        password = dbModel[Users.password],
+        firstname = dbModel[Users.firstname],
+        email = dbModel[Users.email],
+        description = dbModel[Users.description]
+        )
 }
