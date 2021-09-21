5820f1cf9dff06580b48785690e4798b4d4a3a60
Roberto Garcia <groberto74@gmail.com>
2021-09-21 12:05:20 +0200

    Created getUser method in repository auth.

--- src/auth/domain/AuthUserRepositoryImpl.kt
+++ src/auth/domain/AuthUserRepositoryImpl.kt
@@ -5,6 +5,7 @@
 
 package auth.domain
 
+import com.example.rgc.opendotaktor.users.domain.User
 import com.example.rgc.opendotaktor.users.domain.UserRepository
 import com.example.rgc.opendotaktor.users.local.UserLocal
 
@@ -13,8 +14,13 @@ class AuthUserRepositoryImpl(private val userRepository : UserRepository) : Auth
     override suspend fun save(user: UserLocal) {
         userRepository.save(user)
     }
+
+    override suspend fun getByUsername(username: String): User? {
+        return userRepository.getByUsername(username)
+    }
 }
 
 interface AuthUserRepository {
     suspend fun save(user : UserLocal)
+    suspend fun getByUsername(username : String) : User?
 }
