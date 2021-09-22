4abe52abae23fc10108ec219039db84ee4a2b433
Roberto Garcia <groberto74@gmail.com>
2021-09-22 15:01:24 +0200

    Authenticated get heroes route.

--- src/heroes/domain/RoutingHero.kt
+++ src/heroes/domain/RoutingHero.kt
@@ -1,14 +1,17 @@
 package com.example.rgc.opendotaktor.heroes.domain
 
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Routing.heroes(repository : HeroStatsRepository) {
-    get("/heroStats") {
-        val paramPage : String? = call.request.queryParameters["page"]
-        val page : String = if(paramPage === null) "0" else paramPage
-        call.respond(repository.getHeroStats(Integer.parseInt(page)))
+    authenticate("auth-jwt") {
+        get("/heroStats") {
+            val paramPage: String? = call.request.queryParameters["page"]
+            val page: String = if (paramPage === null) "0" else paramPage
+            call.respond(repository.getHeroStats(Integer.parseInt(page)))
+        }
     }
 }
 
